dd050172b1b1e5cf928b10947ca70e3b2088b4f5
Samuel Bejan <110773742+SamiBejan@users.noreply.github.com>
2023-04-02 22:18:14 +0300

    Implemented the "delete" functionality

--- PhoneList.js
+++ PhoneList.js
@@ -1,5 +1,7 @@
 const listArea = document.querySelector(".listArea");
 const addButton = document.querySelector(".addButton");
+const deleteButton = document.querySelector(".deleteButton");
+
 
 const list = new Array();
 const brandList = new Array();
@@ -10,17 +12,29 @@ function add() {
     document.getElementById("Brand").value != "" && document.getElementById("Model").value != "") {
         const row = document.createElement("li");
         row.innerText = document.getElementById("Brand").value + "   " +  document.getElementById("Model").value;
-        for (let i = 0; i < list.length; ++i) {
-            ++list[i].id;
-        }
         list.unshift(row);
-        list[0].id = 1;
+        for (let i = 0; i < list.length; ++i) {
+            list[i].onclick = function () {select(i)};
+            list[i].style.background = "transparent";
+        }
         brandList.unshift(document.getElementById("Brand").value);
         modelList.unshift(document.getElementById("Model").value);
         listArea.prepend(row);
     }
 }
 
+function select(x) {
+    list[x].setAttribute("class", "selected");
+    list[x].style.background = "blue";
+}
+
+function remove() {
+    const selected = document.querySelectorAll(".selected");
+    for (let i = 0; i < selected.length; ++i) {
+        selected[i].remove();
+    }
+}
 
 addButton.addEventListener("click", add);
+deleteButton.addEventListener("click", remove);
 
